c8b490769132c40606a8c6e2422f1c651c55146b
Clare Childs <clarechildstv@hotmail.com>
2015-05-05 20:43:05 +0100

    initial commit

---
+++ js_script1.js
@@ -0,0 +1,160 @@
+// user input for target duration
+var hoursTarget = document.getElementById("hourTarget");
+var minTarget = document.getElementById("minTarget");
+var secTarget = document.getElementById("secTarget");
+var frTarget = document.getElementById("frTarget");
+
+// user input for Part 1
+var hours1 = document.getElementById("hour1");
+var min1 = document.getElementById("min1");
+var sec1 = document.getElementById("sec1");
+var fr1 = document.getElementById("fr1");
+
+// user input for Part 2
+var hours2 = document.getElementById("hour2");
+var min2 = document.getElementById("min2");
+var sec2 = document.getElementById("sec2");
+var fr2 = document.getElementById("fr2");
+
+// user input for Part 3
+var hours3 = document.getElementById("hour3");
+var min3 = document.getElementById("min3");
+var sec3 = document.getElementById("sec3");
+var fr3 = document.getElementById("fr3");
+
+// user input for Part 4
+var hours4 = document.getElementById("hour4");
+var min4 = document.getElementById("min4");
+var sec4 = document.getElementById("sec4");
+var fr4 = document.getElementById("fr4");
+
+// total display area
+var totalHr = document.getElementById("totalHr");
+var totalMin = document.getElementById("totalMin");
+var totalSec = document.getElementById("totalSec");
+var totalFr = document.getElementById("totalFr");
+
+//add part (not currently in use)
+var addPart = document.getElementById("addPartButton");
+
+//making sure result is returned as a number and a string
+function getValue(input){
+  var result = parseInt(input.value, 10);
+  if (isNaN(result) ){
+  	return 0;
+  } else {
+    return result;
+  }
+}
+
+//pushes the result into input form 
+function setValue(input, num){
+  input.value = num;
+}
+
+// converts timecode to frames (number number number number -> number)
+function timecodeToFrames(hours, mins, seconds, frames){
+  var convertHoursToFrames = hours * 60 * 60* 25;
+  var convertMinsToFrames = mins * 60 * 25;
+  var convertSecsToFrames = seconds * 25;
+  return convertHoursToFrames + convertMinsToFrames + convertSecsToFrames + frames;
+}  
+
+
+// converts frames to a timecode and returns an array (number -> number number number number)
+function framesToTimecode(totalFrames){
+  var frames = totalFrames % 25;
+  var seconds = Math.floor(totalFrames / 25) % 60;
+  var minutes = Math.floor(totalFrames /60 / 25) % 60;
+  var hours = Math.floor(totalFrames / 60 / 60 / 25); 
+  return [hours , minutes, seconds, frames];
+} 
+
+function compute(){
+  //get user target duration values 
+  var ht = getValue(hourTarget);
+  var mt = getValue(minTarget);
+  var st = getValue(secTarget);
+  var ft = getValue(frTarget);
+
+  //get part 1 values
+  var h1 = getValue(hours1);
+  var m1 = getValue(min1);
+  var s1 = getValue(sec1);
+  var f1 = getValue(fr1);
+
+  //get part 2 values
+  var h2 = getValue(hours2);
+  var m2 = getValue(min2);
+  var s2 = getValue(sec2);
+  var f2 = getValue(fr2);
+
+  //get part 3 values
+  var h3 = getValue(hours3);
+  var m3 = getValue(min3);
+  var s3 = getValue(sec3);
+  var f3 = getValue(fr3);
+
+  //get part 4 values
+  var h4 = getValue(hours4);
+  var m4 = getValue(min4);
+  var s4 = getValue(sec4);
+  var f4 = getValue(fr4);
+
+  //converts parts 1-4 from timecode to frames 
+  var tc1 = timecodeToFrames(h1, m1, s1, f1);
+  var tc2 = timecodeToFrames(h2, m2, s2, f2); 
+  var tc3 = timecodeToFrames(h3, m3, s3, f3); 
+  var tc4 = timecodeToFrames(h4, m4, s4, f4);
+
+  //converts target timecode into frames
+  var targetTC = timecodeToFrames(ht, mt, st, ft);
+
+  //adds the totals of all parts (in frames) and returns result as an array
+  var totalArray = framesToTimecode(tc1 + tc2 + tc3 + tc4); 
+
+  // subtracts the target timecode from the total duration (pts 1-4)
+  var diffArray = framesToTimecode(targetTC - (tc1 + tc2 + tc3 + tc4)); 
+
+  //sets the value of the 'totalArray' elements into the (yellow) total form
+  setValue(totalHr, totalArray[0]);
+  setValue(totalMin, totalArray[1]);
+  setValue(totalSec, totalArray[2]);
+  setValue(totalFr, totalArray[3]);
+
+  //sets the value of the 'diffArray' elements into the (bottom) total form
+  setValue(diffHr, diffArray[0]);
+  setValue(diffMin, diffArray[1]);
+  setValue(diffSec, diffArray[2]);
+  setValue(diffFr, diffArray[3]);
+
+  //change this form to return the difference result as a string and array
+}
+
+$(document).ready(function(){
+  $ ('#calcWrapper').draggable();
+  $('#overUnderBy').prepend("<p>This is a string</p>");
+});
+
+
+/*
+
+function difference(num1, num2){  
+  if(targetDur < totalDur){
+    var diffOfTotalAndTarget = (totalDur - targetDur);
+    console.log("You are OVER by +" + diffOfTotalAndTarget);
+  }else if(targetDur > totalDur){
+    diffOfTotalAndTarget = (targetDur- totalDur);
+    console.log("You are UNDER by -" + diffOfTotalAndTarget);    
+  }else{
+    console.log("BOOM target dur of "+ targetDur +" is reached!");
+  }  
+}
+
+var targetDur = 20;
+var totalDur = 20;
+
+difference(targetDur, totalDur);
+
+*/
+
